cf68d1a7e8917a7e6d5b78063a1891380ba3c584
James Heinemann <70541819+jheinem1@users.noreply.github.com>
2025-05-15 11:08:15 -0700

    Use Bling (ublue installer repository) to install 1Password & CLI

--- build.sh
+++ build.sh
@@ -5,12 +5,7 @@ basearch=${basearch:-$(uname -m)}
 
 ### Install packages
 
-# Add 1Password repo
-wget https://downloads.1password.com/linux/rpm/stable/x86_64/1password-latest.rpm
-
 # Install packages with dnf5
-dnf5 install -y ./1password-latest.rpm
-dnf5 install -y 1password-cli
 dnf5 install -y konsole
 dnf5 install -y piper
 dnf5 install -y yakuake
