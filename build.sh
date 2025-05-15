7fad539baa4ed363f9276240153ff49ae8d04d43
jhein <jheinem01@icloud.com>
2025-05-14 19:42:26 -0700

    Use the 1Password installation commands directly

--- build.sh
+++ build.sh
@@ -6,7 +6,8 @@ basearch=${basearch:-$(uname -m)}
 ### Install packages
 
 # Add 1Password repo
-dnf5 -y config-manager addrepo --from-repofile=https://downloads.1password.com/linux/rpm/stable/$basearch/1password.repo
+sudo rpm --import https://downloads.1password.com/linux/keys/1password.asc
+sudo sh -c 'echo -e "[1password]\nname=1Password Stable Channel\nbaseurl=https://downloads.1password.com/linux/rpm/stable/\$basearch\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=\"https://downloads.1password.com/linux/keys/1password.asc\"" > /etc/yum.repos.d/1password.repo'
 
 # Install packages with dnf5
 dnf5 install -y 1password
