d4374daf8c3cc1fa819eb30d1b9f232533a64c79
James Heinemann <70541819+jheinem1@users.noreply.github.com>
2025-06-24 08:45:50 -0700

    Enable RPM Fusion repo for Discord

--- build.sh
+++ build.sh
@@ -37,6 +37,13 @@ EOF
 rpm --import https://packages.microsoft.com/keys/microsoft.asc
 
 ###############################################################################
+# RPM Fusion repos for additional packages such as Discord
+###############################################################################
+dnf5 install -y \
+  https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
+  https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
+###############################################################################
 # Gamescope Git repo from COPR
 ###############################################################################
 # Install the copr plugin then enable the gamescope-git repo
